51fba0e27482f692a8ed5f04736337099f24d3a9
Fabrizio <fabrizio.nastri@flexup.org>
2023-07-29 13:01:03 +0200

    fixed OrderList

--- src/adapters/front/vue/src/components/OrderList.vue
+++ src/adapters/front/vue/src/components/OrderList.vue
@@ -2,38 +2,10 @@
   <h2>Order Datas</h2>
   <ul>
     <li v-for="order in orders" :key="order.id">
-      <h3>Order: {{ order.name }}</h3>
-      <ul>
-        <li>Order ID: {{ order.id }}</li>
-        <li>Client ID: {{ order.clientId }}</li>
-        <li>Supplier ID: {{ order.supplierId }}</li>
-        <li>Amount excluding tax: {{ order.amountExclTax }}</li>
-        <li>Tax amount: {{ order.taxAmount }}</li>
-        <li>Amount including tax: {{ order.amountInclTax }}</li>
-        <li>Average tax rate: {{ order.averageTaxRate }}</li>
-        <li>Principal: {{ order.principal }}</li>
-      </ul>
-      <h4 style="text-decoration: underline">Items:</h4>
-      <ul>
-        <li v-for="item in order.items" :key="item.id">
-          <h5>Item: {{ item.name }}</h5>
-          <ul>
-            <li>Item ID: {{ item.id }}</li>
-            <li>Order ID: {{ item.orderId }}</li>
-            <li>Quantity: {{ item.quantity }}</li>
-            <li>Unit: {{ item.unit }}</li>
-            <li>Unit price: {{ item.unitPriceExclTax }}</li>
-            <li>Tax rate: {{ item.taxRate * 100 }}%</li>
-            <li>Unit price including tax: {{ item.unitPriceInclTax }}</li>
-            <li>Amount excluding tax: {{ item.amountExclTax }}</li>
-            <li>Tax amount: {{ item.taxAmount }}</li>
-            <li>Amount including tax: {{ item.amountInclTax }}</li>
-          </ul>
-        </li>
-      </ul>
-      <pre>
+      <OrderDetails :order="order" />
+      <!-- <pre>
         {{ JSON.stringify(order, null, 2) }}
-      </pre>
+      </pre> -->
     </li>
   </ul>
 </template>
@@ -42,6 +14,7 @@
 import { ref, onMounted, watch } from 'vue'
 import { fetchOrders } from '../composables/fetchOrders'
 import { sharedState } from '../sharedState'
+import OrderDetails from './OrderDetails.vue'
 
 const orders = ref(null)
 
